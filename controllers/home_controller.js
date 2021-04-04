404a4fdbcda08b19e22befec53413cc3e3a786b0
Samiran Roy <samiranroy0407@gmail.com>
2021-04-04 14:53:25 +0530

    minor problem fixed

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -2,9 +2,18 @@ const User = require('../models/user');
 
 module.exports.home = async function(req, res){
 
-    let user = await User.findById(req.user._id);
+    try{
+        let user
 
-    return res.render('home', {
-        user: user
-    });
+        if(req.user)
+            user = await User.findById(req.user._id);
+
+        return res.render('home', {
+            user: user
+        });
+    }
+    catch(err){
+        console.log(err);
+        return;
+    }
 }
